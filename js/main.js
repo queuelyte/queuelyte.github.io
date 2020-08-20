05d27da596c121a7d94b3b24c3be69817ae552e8
queuelyte <queuelyte@gmail.com>
2020-08-20 13:43:29 +0530

    resposive navigation bar added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 
 function  appLink(){
     window.location.href ="https://www.google.com";
+    if(isOpen == true){
+        list.style.left = "-100%";
+        isOpen = !isOpen;
+    }
 }
+
+window.addEventListener('scroll',function(){
+  if(isOpen == true){
+      list.style.left = "-100%";
+      isOpen = !isOpen;
+  }
+});
 
 var btn = document.getElementById("goToFooter");
 
@@ -11,4 +22,40 @@ btn.addEventListener("click",function(){
         left: 0,
         behavior:"smooth"
     });
+
+    if(isOpen == true){
+        list.style.left = "-100%";
+        isOpen = !isOpen;
+    }
+});
+
+var navBtn = document.getElementById("bars");
+var isOpen = new Boolean(true);
+
+navBtn.addEventListener("click",function(){
+    isOpen = !isOpen;
+    var list = document.getElementById("list")
+    console.log(isOpen);
+    if(isOpen){
+        list.style.left = 0;
+        list.style.width = "100%";
+    }else{
+        list.style.left = "-100%";
+    }
+});
+
+var body = document.getElementById("mainbody");
+body.addEventListener("click",function(){
+    if(isOpen == true){
+        list.style.left = "-100%";
+        isOpen = !isOpen;
+    }
+});
+
+var home = document.getElementById("home");
+home.addEventListener("click",function(){
+    if(isOpen == true){
+        list.style.left = "-100%";
+        isOpen = !isOpen;
+    }
 });
